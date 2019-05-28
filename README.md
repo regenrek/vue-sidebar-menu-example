0f51e8ed5fbbff33ecf5451c20787b97e4716cd4
kevin regenrek <hello@regenrek.at>
2019-05-28 10:01:02 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-sidebar-menu-example
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <nav class="main-nav">
+      <div class="logo">my.company</div>
+      <Burger></Burger>
+    </nav>
+
+    <Sidebar>
+      <ul class="sidebar-panel-nav">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </Sidebar>
+  </div>
+</template>
+
+<script>
+import Burger from "./components/Menu/Burger.vue";
+import Sidebar from "./components/Menu/Sidebar.vue";
+
+export default {
+  name: "app",
+  components: {
+    Burger,
+    Sidebar
+  }
+};
+</script>
+<style>
+html {
+  height: 100%;
+  overflow: hidden;
+}
+
+body {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  font-family: "Lato";
+  height: 100%;
+  background: rgb(101, 31, 87);
+  background: linear-gradient(
+    45deg,
+    rgba(101, 31, 87, 1) 0%,
+    rgba(225, 113, 87, 1) 48%,
+    rgba(249, 248, 113, 1) 100%
+  );
+}
+
+.logo {
+  align-self: center;
+  color: #fff;
+  font-weight: bold;
+  font-family: "Lato";
+}
+
+.main-nav {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 0.8rem;
+}
+
+ul.sidebar-panel-nav {
+  list-style-type: none;
+}
+
+ul.sidebar-panel-nav > li > a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: block;
+  padding-bottom: 0.5em;
+}
+</style>
